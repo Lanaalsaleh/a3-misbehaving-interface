0b120134696cf01b31dd4f165bf26538cec27ba9
lanaalsaleh <vn25xf@brocku.ca>
2026-05-21 17:52:59 -0400

    loading screen time chnaged

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// so first idea was to make a sign up screen to win a gift card but make it take so long to load i got this idea from real life becuase websites usually take so long to load almost everytime
 // get button
 const startBtn = document.getElementById("startBtn");
 
@@ -7,7 +8,8 @@ startBtn.addEventListener("click", function() {
     // change text 
     document.body.innerHTML = `<h1 style="color:white;">Loading...</h1>`;
 
-    // wait 3 seconds
+    // wait 10 seconds
+    // i tried 3 secs at first but felt too short so i made it 10 might even make it longer 
     setTimeout(function() {
         // transfer to signup page
         document.body.innerHTML = ` 
@@ -19,7 +21,7 @@ startBtn.addEventListener("click", function() {
         <button id="submitBtn">Submit</button>
         </div>
         `;
-    }, 3000);
+    }, 15000);
     });
 
         
